b9ddaab246f22986bb479cb4edef529cf3b0ee88
Firda <nikita.sv2000@gmail.com>
2024-01-26 17:20:35 +0200

    Додано новий проект ClassWork2.2.js до ClassWork

---
+++ ClassWork/ClassWork2.2.js
@@ -0,0 +1,11 @@
+let map1 = new Map()
+
+map1.set("name","Vadym")
+map1.set("age",20)
+map1.set("status",true)
+
+console.log(map1)
+console.log(map1.get("name"))
+console.log(map1.keys())
+console.log(map1.values())
+
--- HomeWork/HomeWork2.1.js
+++ HomeWork/HomeWork2.1.js
@@ -1,5 +1,5 @@
 function sendSMS(SMS){
     let result = Math.ceil(SMS.length / 60)
-    console.log(`Щоб надіслати цей текст вам потрібно ${result} повідомлення \n${SMS}`);
+    console.log(`Щоб надіслати цей текст вам потрібно використати ${result} повідомлення(-ь) \n${SMS}`);
 }
 sendSMS("...");
