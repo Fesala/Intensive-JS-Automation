1fec7d1b0011f02c745d42e86d128cef4db1ad33
Firda <nikita.sv2000@gmail.com>
2024-01-29 12:49:25 +0200

    Додав ще одину перевірку в проект HomeWork2.2.js

--- HomeWork/HomeWork2.2.js
+++ HomeWork/HomeWork2.2.js
@@ -8,7 +8,7 @@ function longestPrix(longestPrixArr){
         console.log(longestPrixArr[0]);
         return longestPrixArr[0];
     }
-    
+
     let result = "";
     for (let n = 0; n < longestPrixArr[0].length; n++) {
         for (let k = 1; k < longestPrixArr.length; k++){
@@ -27,3 +27,5 @@ let longestPrixArr1 = ["flower", "flow", "flight"]
 longestPrix(longestPrixArr1);
 let longestPrixArr2 = ["dog", "racecar", "car"]
 longestPrix(longestPrixArr2);
+let longestPrixArr3 = ["flower", "flowe", "flowe"]
+longestPrix(longestPrixArr3);
